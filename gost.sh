bcc99f4fca932a449b3cd075f4a294fe6d533c8e
Salem <39432507+lusalem@users.noreply.github.com>
2025-09-16 16:03:39 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -99,7 +99,6 @@ function Install_ct() {
     rm -rf gost_"$ct_new_ver"_linux_"$bit".tar.gz
     wget --no-check-certificate https://gotunnel.oss-cn-shenzhen.aliyuncs.com/gost_"$ct_new_ver"_linux_"$bit".tar.gz
     tar zxf gost_"$ct_new_ver"_linux_"$bit".tar.gz
-    mv gost_"$ct_new_ver"_linux_"$bit" gost
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
     wget --no-check-certificate https://gotunnel.oss-cn-shenzhen.aliyuncs.com/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
@@ -108,8 +107,7 @@ function Install_ct() {
     rm -rf gost_"$ct_new_ver"_linux_"$bit".tar.gz
     wget --no-check-certificate https://github.com/go-gost/gost/releases/download/v"$ct_new_ver"/gost_"$ct_new_ver"_linux_"$bit".tar.gz
     tar zxf gost_"$ct_new_ver"_linux_"$bit".tar.gz
-    mv gost_"$ct_new_ver"_linux_"$bit" gost
-    mv gost /usr/bin/gost
+	mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
     wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
     mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
